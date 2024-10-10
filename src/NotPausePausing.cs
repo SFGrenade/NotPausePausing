b52273a6e8310357176d785dd7c36ad77db6664d
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-10-10 20:34:23 +0200

    initial commit

---
+++ src/NotPausePausing.cs
@@ -0,0 +1,76 @@
+﻿using JetBrains.Annotations;
+using Modding;
+using UnityEngine;
+
+namespace NotPausePausing;
+
+[UsedImplicitly]
+public class PauseHandler : MonoBehaviour
+{
+    private bool paused = false;
+    private bool frameAdvance = false;
+
+    private void Update()
+    {
+        //Modding.Logger.LogDebug($"[PauseHandler] - Update()");
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            //Modding.Logger.LogDebug($"[PauseHandler] - Update() - P pressed - {!paused} - {Time.timeScale}");
+            paused = !paused;
+            if (paused)
+            {
+                Time.timeScale = 0f;
+            } else
+            {
+                Time.timeScale = 1f;
+                //lastTimescale = 1f;
+            }
+        }
+        if (paused && Input.GetKeyDown(KeyCode.O))
+        {
+            //Modding.Logger.LogDebug($"[PauseHandler] - Update() - O pressed");
+            frameAdvance = true;
+            Time.timeScale = 1f;
+        }
+        if (paused && !frameAdvance)
+        {
+            Time.timeScale = 0f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Modding.Logger.LogDebug($"[PauseHandler] - FixedUpdate()");
+        if (frameAdvance)
+        {
+            //Modding.Logger.LogDebug($"[PauseHandler] - FixedUpdate() - Frame advanced");
+            frameAdvance = false;
+            Time.timeScale = 0f;
+        }
+    }
+}
+
+public class NotPausePausing : Mod
+{
+    public NotPausePausing() : base("Not Pause Pausing")
+    {
+        On.GameManager.Start += OnGameManagerStart;
+    }
+
+    private void OnGameManagerStart(On.GameManager.orig_Start orig, GameManager self)
+    {
+        orig(self);
+        self.gameObject.AddComponent<PauseHandler>();
+    }
+
+    public override void Initialize()
+    {
+        //if (GameManager.instance != null)
+        //{
+        //    if (GameManager.instance.gameObject.GetComponent<PauseHandler>() == null)
+        //    {
+        //        GameManager.instance.gameObject.AddComponent<PauseHandler>();
+        //    }
+        //}
+    }
+}
